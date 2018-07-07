7457acee5d91b36e0c3c2a3bdebbfb375c01b226
Nawej <bianzamutombo@gmail.com>
2018-07-07 12:44:22 +0100

    well done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const FancyButton = (props) => (
+  <button
+    className="my-fancy-classname"
+    onClick={props.handleClick}
+  >
+    Click Me!
+  </button>
+)
+// const LogWhenClicked=(props)=>(
+//   <button
+//   className="logWhenClicked"
+//   onclik ={props.handleClick}
+//   >
+//    Click Me!
+//   </button>
+
+// )
 class App extends Component {
+  logWhenClicked = () => {
+    console.log ('my live')
+  }
   render() {
-     const Logo=()=>(
+    
+    const Logo =(props)=>(  
      <header className="App-header">
-     <img src="https://codeyourfuture.io/static/media/cyf_brand.fbcea877.png"className="App-logo" alt="logo" />
+     <img src="https://codeyourfuture.io/static/media/cyf_brand.fbcea877.png"className="App-logo" onClick={this.logWhenClicked} alt = "logo" />
      <h1 className="App-title">Welcome to cyf hotel</h1>
    </header>
-    );
-
-   const BookingsMessage=()=>(
+   );
+   
+    class BookingsMessage extends Component {
+      render() {
+      return(
+  //  const BookingsMessage=()=>(
     <p className="App-intro">
     there are bookings available for the {new Date().toLocaleDateString()}
     </p>
-   )
-    const SpecialDeals=()=>{
+  //  )
+    )
+  }
+}
+    const SpecialDeals=(props)=>{
      const specialDealstrings=['fruit','mango', 'orange','bananas']
      return(
      <div>
@@ -25,14 +52,18 @@ class App extends Component {
     ))} 
     </div>)  
   };
+
     return (
       <div className="App">
         <Logo/>
+        {/* <LogWhenClicked/> */}
         <BookingsMessage/>
         <SpecialDeals/>
+        <FancyButton handleClick={this.logWhenClicked} />
       </div>
     );
   }
 }
 
 export default App;
+
